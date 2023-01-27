feb4cb77036f60882ee49a8bf784df83050bd072
Shaily Priyadarshini <priyadarshinishaily@gmail.com>
2023-01-27 18:48:44 +0530

    Edge case added.

--- script.js
+++ script.js
@@ -1,39 +1,47 @@
 'use strict';
 let value=Math.trunc(Math.random()*20)+1;
-console.log(document.querySelector('.check').addEventListener('click',function(){
+//function 
+
+document.querySelector('.check').addEventListener('click',function(){
     let num=Number(document.querySelector('.ip-number').value);
     let score=Number(document.querySelector('.score').textContent);
     let highscore=Number(document.querySelector('.highscore').textContent);
-    if (num>value){
-        document.querySelector('.state').textContent="Too High!!!";
+    const change=function(){
         score=score-1
         document.querySelector('.score').textContent=`${score}`;
         document.querySelector('.ip-number').value="";
-
     }
+    if(num>20){
+        document.querySelector('.state').textContent="Out of range!!!";
+        change();
+    }
+
+    else if (num>value){
+        document.querySelector('.state').textContent="Too High!!!";
+        change();
+    }
+   
     else if(value>num){
         document.querySelector('.state').textContent="Too Low!!!";
-        score=score-1
-        document.querySelector('.score').textContent=`${score}`;
-        document.querySelector('.ip-number').value="";
+        change();
     
-
     }
     else
     {
         document.querySelector('.state').textContent="🎉 correct answer";
-        document.body.style.backgroundColor='green';
-        document.querySelector('.ip-number').style.backgroundColor='yellow';
+        document.body.style.backgroundColor='#101820FF';
+        document.querySelector('.ip-number').style.backgroundColor='#FEE715FF ';
         highscore=highscore>score?highscore:score;
         document.querySelector('.highscore').textContent=`${highscore}`;
         document.querySelector('.number').textContent=`${num}`;
     }
-}))
-console.log(document.querySelector('.again').addEventListener('click',function(){
+})
+document.querySelector('.again').addEventListener('click',function(){
     value=Math.trunc(Math.random()*20)+1;
     let score=(document.querySelector('.score').textContent=20);
     document.body.style.backgroundColor=' rgb(41, 39, 39)';
     document.querySelector('.state').textContent="Start guessing...";
     document.querySelector('.ip-number').value="";
     document.querySelector('.number').textContent="?";
-}))
+    document.querySelector('.ip-number').style.backgroundColor=' rgb(41, 39, 39)';
+})
